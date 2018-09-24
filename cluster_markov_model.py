64c9ad195045df777e0b0bcfcc8dcff3dd5bbd29
josmfred <josmfred@hotmail.com>
2018-09-23 18:31:43 -0700

    first commit

---
+++ cluster_markov_model.py
@@ -0,0 +1,24 @@
+import pickle
+from sklearn.externals import joblib
+import numpy as np
+
+
+def main():
+    parent_texts = pickle.load(open("parent_texts.pkl", "rb"))
+    child_texts = pickle.load(open("children_texts.pkl", "rb"))
+    cluster_model = joblib.load(open("cluster_comments.pkl", "rb"))
+    tokenizer = pickle.load(open("tokenizer.pkl", "rb"))
+    tfidf_parent_texts = tokenizer.texts_to_matrix(parent_texts,
+                                                    mode='tfidf')
+    children_tokened = tokenizer.texts_to_sequences(child_texts)
+    predicts = cluster_model.predict(tfidf_parent_texts)
+    cluster_markov_chain = {k : np.arange(0, 10000) for k in range(256)}
+    for cluster, child in zip(predicts, children_tokened):
+        for word in child:
+            if word != 0:
+                cluster_markov_chain[cluster][word] += 1
+    p_chain = {k : (lambda d: d / d.sum())(v) for k, v in cluster_markov_chain.items()}
+    pickle.dump(p_chain, open("cluster_markov_model.pkl", "wb"))
+
+if __name__ == "__main__":
+    main()
---
+++ predict_upvotes_model.py
@@ -0,0 +1,48 @@
+import pickle
+import numpy as np
+from keras import layers, models
+
+
+def make_upvote_model(score_data_shape, text_data_shape, neurons, vocab_size):
+    score_input = layers.Input(shape=score_data_shape,
+                               dtype='float32', name="score_input")
+    text_input = layers.Input(shape=text_data_shape,
+                              dtype='float32', name="text_input")
+    embedding = layers.Embedding(vocab_size, neurons[0])(text_input)
+    text_lstm = layers.LSTM(neurons[1])(embedding)
+    concate = layers.concatenate([score_input, text_lstm])
+    dense = layers.Dense(neurons[2], activation="relu")(concate)
+    main_output = layers.Dense(1, activation="relu",
+                               name="main_output")(dense)
+    model = models.Model(inputs=[score_input, text_input],
+                         outputs=[main_output])
+    return model
+
+
+def compile_model(model, opt, loss, metrics):
+    model.compile(
+        optimizer=opt,
+        loss=loss,
+        metrics=metrics
+    )
+    return model
+
+
+def main():
+    texts = np.load("comment_texts.npy")
+    scores = np.load("comment_sentiment_scores.npy")
+    upvotes = np.load("comment_upvotes.npy")
+
+    epochs = 3
+    neurons = (16, 32, 64)
+    model = make_upvote_model(scores.shape[1:],
+                              texts.shape[1:], neurons, 10000)
+    model = compile_model(model, "adam", "mse", ["mse"])
+    hst = model.fit([scores, texts], [upvotes],
+                epochs=epochs, batch_size=32, validation_split=0.1,
+                shuffle=True, verbose=True).history
+
+    model.save("predict_upvotes.h5")
+
+if __name__ == "__main__":
+    main()
